d2392859e95cc08369fa88320232033f21c4fad3
John-Kim <Leftium@users.noreply.github.com>
2022-05-03 14:51:12 +0900

    Get list of members signed up.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
+const GCP_API_KEY    = import.meta.env.VITE_GCP_API_KEY
+const SPREADSHEET_ID = import.meta.env.VITE_SPREADSHEET_ID
+
+const URL_FORM = 'https://docs.google.com/forms/d/e/1FAIpQLSf1v-qc7z0hCY-_izfUH7sYU4AZNvyesCC9-V1LmjdaVZJJig/viewform'
+const URL_SHEETS = `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/%EC%84%A4%EB%AC%B8%EC%A7%80+%EC%9D%91%EB%8B%B5+%EC%8B%9C%ED%8A%B81?majorDimension=ROWS&key=${GCP_API_KEY}`
+
 export async function get() {
-    const URL_FORM = 'https://docs.google.com/forms/d/e/1FAIpQLSf1v-qc7z0hCY-_izfUH7sYU4AZNvyesCC9-V1LmjdaVZJJig/viewform'
-
-
     let resp = await fetch(URL_FORM);
     let text = await resp.text();
 
@@ -11,9 +14,38 @@ export async function get() {
         title = matches[1];
     }
 
+    resp = await fetch(URL_SHEETS);
+    let json = await resp.json();
+
+    json.values.shift();  // Skip labels.
+
+    let members = [];
+    let numMen = 0;
+    let numWomen = 0;
+    let numTotal = json.values.length;
+
+    for (const item of json?.values) {
+        let name = item[2];
+        let sex  = item[5];
+
+
+        if (sex == '남(men)') {
+            numMen++;
+        }
+        if (sex == '여(women)') {
+            numWomen++;
+        }
+
+        members.push({ name, sex });
+    }
+
     return {
         body: {
-            title
+            title,
+            numTotal,
+            numMen,
+            numWomen,
+            members
         }
     }
 
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,18 @@
 <script>
     export let title;
+    export let numTotal;
+    export let numMen;
+    export let numWomen;
+    export let members;
 </script>
 
 
-{title}
+<h1>{title}</h1>
+
+Total: {numTotal}
+
+Men: {numMen}
+
+Women: {numWomen}
+
+<pre>{JSON.stringify(members, null, 4)}</pre>
