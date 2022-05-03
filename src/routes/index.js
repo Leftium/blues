c214167ea28847d0e9b0780948edf414483ab696
John-Kim <Leftium@users.noreply.github.com>
2022-05-03 14:14:53 +0900

    Grab title from form page.

---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+export async function get() {
+    const URL_FORM = 'https://docs.google.com/forms/d/e/1FAIpQLSf1v-qc7z0hCY-_izfUH7sYU4AZNvyesCC9-V1LmjdaVZJJig/viewform'
+
+
+    let resp = await fetch(URL_FORM);
+    let text = await resp.text();
+
+    let title = '';
+    let matches = text.match(/<title>(.*)<\/title>/);
+    if (matches) {
+        title = matches[1];
+    }
+
+    return {
+        body: {
+            title
+        }
+    }
+
+}
