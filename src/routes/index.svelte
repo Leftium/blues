d1ea91eca8419b98204d52198ea2e02bad2df35e
John-Kim <Leftium@users.noreply.github.com>
2022-05-06 12:13:05 +0900

    Add title.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,7 +7,8 @@
 </script>
 
 <svelte:head>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>{title}</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </svelte:head>
 
 <main>
