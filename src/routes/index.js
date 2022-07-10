85eb4e4ef7b54c08f6d70b914153ff9f09b9c9e2
John-Kim <Leftium@users.noreply.github.com>
2022-07-10 22:59:08 +0900

    Add 사슴이다.

--- src/routes/index.js
+++ src/routes/index.js
@@ -130,6 +130,7 @@ export async function get({ url }) {
             좐:      '/img/special/john.jpg',
             쩐주:     '/img/special/쩐주.jpg',
             사슴:     '/img/special/사슴.jpg',
+            사슴이다:  '/img/special/사슴.jpg',
             리스:     '/img/special/리스.jpg',
         }
 
