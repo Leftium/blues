5ba2c8161def942d0addaf486f1c4048d33e1828
John-Kim <Leftium@users.noreply.github.com>
2022-07-10 10:18:28 +0900

    Add more icons.

--- src/routes/index.js
+++ src/routes/index.js
@@ -128,6 +128,9 @@ export async function get({ url }) {
             수:      '/img/special/sue.jpg',
             sue:     '/img/special/sue.jpg',
             좐:      '/img/special/john.jpg',
+            쩐주:     '/img/special/쩐주.jpg',
+            사슴:     '/img/special/사슴.jpg',
+            리스:     '/img/special/리스.jpg',
         }
 
         if (testId && i == json?.values?.length-1) {
