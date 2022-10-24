12c890d7f35b4f4a5590479059eca55f156d5376
John-Kim <Leftium@users.noreply.github.com>
2022-10-25 02:00:40 +0900

    Revert "Add server rendered images."
    This reverts commit 0fc95c4401c5538673d0322610f7c5d7ee2a44da.

--- src/routes/satori/index.js
+++
@@ -1,7 +0,0 @@
-import { processUrl } from '$lib/common.js'
-
-export async function GET({ url }) {
-
-    let body = await processUrl(url)
-    return { body }
-}
--- src/routes/satori/index.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import AvatarList from '$lib/components/AvatarList.svelte'
-    import Simple from '$lib/components/Simple.svelte'
-
-    export let members;
-
-</script>
-
-<AvatarList {members} />
